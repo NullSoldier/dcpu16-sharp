021ebb992041e81c3c14523f07d44598576f9044
Jason Spafford <nullprogrammer@gmail.com>
2012-11-17 23:35:18 -0800

    Added the Tests projects and changed namespaces

--- src/Enums.cs
+++ src/Enums.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DCPU16Emulator
+namespace dcpu16sharp
 {
 	// ReSharper disable InconsistentNaming
 	public enum OPCODES
--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DCPU16Emulator
+namespace dcpu16sharp
 {
 	public class Program
 	{
